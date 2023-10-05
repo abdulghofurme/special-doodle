d64c4231abb0004070f87ff80c5eec7ad16b8eb4
Abdul Ghofur <abdulghofur.me@gmail.com>
2023-10-05 14:53:13 +0700

    feature(users): Create User

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/abdulghofurme/special-doodle/database"
+	"github.com/abdulghofurme/special-doodle/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,11 +12,17 @@ func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to my awesome API")
 }
 
+func setupRoutes(app *fiber.App) {
+	app.Get("/api", welcome)
+
+	app.Post("/api/users", routes.CreateUser)
+}
+
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/api", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
---
+++ routes/user.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"github.com/abdulghofurme/special-doodle/database"
+	"github.com/abdulghofurme/special-doodle/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	// this is not the model User, seet his as the serializer
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func CreateResponseUser(userModel *models.User) User {
+	return User{
+		ID:        userModel.ID,
+		FirstName: userModel.FirstName,
+		LastName:  userModel.LastName,
+	}
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&user)
+	responseUser := CreateResponseUser(&user)
+
+	return c.Status(200).JSON(&responseUser)
+}
